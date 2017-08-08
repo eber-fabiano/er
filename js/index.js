b81902a6cf1dafc1cdedd226b5badb2b40e6d385
Eber Fabiano <eber.fabiano.pacanhela@gmail.com>
2017-08-08 00:34:49 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.js--triggerSim').click(function(e){
+        e.preventDefault();
+        
+        var show = $('.js--show');
+        show.addClass('show');            
+    });
+});
